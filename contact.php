638f2725fd5da02ccb580444bc676d529d388a89
Unknown <clayton@berclay.co.nz>
2017-11-02 02:18:07 +1300

    Formed additional pages & Created contact form
    Contact form requires tweaking

---
+++ contact.php
@@ -0,0 +1,318 @@
+<?php
+	include "config.php";
+	include "library/functions.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KJGS4HC');</script>
+<!-- End Google Tag Manager -->
+<title>Real Estate 7</title>
+
+<!-- Bootstrap -->
+<link rel="stylesheet" type="text/css" media="screen" href="css/bootstrap.css">
+<link rel="stylesheet" type="text/css" media="screen" href="css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" media="screen" href="css/bootstrap-theme.css">
+<link rel="stylesheet" type="text/css" media="screen" href="css/bootstrap-theme.min.css">
+<link rel="stylesheet" type="text/css" media="screen" href="css/bootstrap-social.css">
+<!-- Font CSS -->
+<link rel="stylesheet" type="text/css" media="screen" href="css/font-awesome.css">
+<link rel="stylesheet" type="text/css" media="screen" href="css/font-awesome.min.css">
+<!-- Custom CSS -->
+<link rel="stylesheet" type="text/css" media="screen" href="css/style.css">
+
+
+
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KJGS4HC"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid"> 
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#myInverseNavbar2" aria-expanded="false"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+      <a class="navbar-brand" href="http://realestate7.serverup.xyz/?i=1"><img class="img-responsive" src="img/logo.jpg" alt="thumb"> </a> </div>
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="myInverseNavbar2">
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#"><span class="glyphicon glyphicon-user"></span> <strong>Sign Up</strong></a></li>
+      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> <strong>Login</strong></a></li>
+      <li class="visible-xs-inline"><a href="index.php">Home</a></li>
+      <li class="visible-xs-inline"><a href="buy.php">Buying</a></li>
+      <li class="visible-xs-inline"><a href="sell.php">Selling</a></li>
+      <li class="visible-xs-inline"><a href="rent.php">Renting</a></li>
+      <li class="visible-xs-inline"><a href="finance.php">Finance</a></li>
+      <li class="visible-xs-inline"><a href="contact.php">Contact Us</a></li>
+    </ul>
+    </div>
+    <!-- /.navbar-collapse --> 
+  </div>
+  <!-- /.container-fluid --> 
+	</nav>
+	<div class="container">
+	<div class="row">
+	  <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 hidden-xs">
+		<div id="carousel-299058" class="carousel slide">		  
+		  <div class="carousel-inner">
+			<div class="item"> <img class="img-responsive" src="img/slider-1.jpg" alt="thumb">
+			  <div class="carousel-299058-caption"> Buy your dream home today! </div>
+			</div>
+			<div class="item active"> <img class="img-responsive" src="img/slider-2.jpg" alt="thumb">
+			  <div class="carousel-299058-caption"> Dreams may not be free but they are worth it! </div>
+			</div>
+			<div class="item"> <img class="img-responsive" src="img/slider-3.jpg" alt="thumb">
+			  <div class="carousel-299058-caption"> Let us help you find your perfect home. </div>
+			</div>
+		  </div>
+		  <a class="left carousel-control" href="#carousel-299058" data-slide="prev"><span class="icon-prev"></span></a> <a class="right carousel-control" href="#carousel-299058" data-slide="next"><span class="icon-next"></span></a></div>
+		<nav>
+			<div id="main_navigation">
+				<ul class="menu">
+					<li class="current"><a href="index.php">Main</a></li>
+					<li><a href="buy.php">Buying</a></li>
+					<li><a href="sell.php">Selling</a></li>
+					<li><a href="rent.php">Renting</a></li>
+					<li><a href="finance.php">Finance</a></li>
+					<li><a href="contact.php">Contact Us</a></li>
+				</ul>
+			</div>
+     	</nav>
+      </div>    
+    </div>
+    <hr>
+  </div>
+<section>
+  <div class="container">
+</section>
+<hr>
+<div class="container">
+  <div class="row">
+    <div class="col-lg-9 col-md-12">
+
+    <form class="well form-horizontal" action="<a href="mailto:sales@berclay.co.nz>
+<fieldset>
+
+<!-- Form Name -->
+<legend>Contact Us Today!</legend>
+
+<!-- Text input-->
+
+<div class="form-group">
+  <label class="col-md-4 control-label">First Name</label>  
+  <div class="col-md-4 inputGroupContainer">
+  <div class="input-group">
+  <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+  <input  name="first_name" placeholder="First Name" class="form-control"  type="text">
+    </div>
+  </div>
+</div>
+
+<!-- Text input-->
+
+<div class="form-group">
+  <label class="col-md-4 control-label" >Last Name</label> 
+    <div class="col-md-4 inputGroupContainer">
+    <div class="input-group">
+  <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+  <input name="last_name" placeholder="Last Name" class="form-control"  type="text">
+    </div>
+  </div>
+</div>
+
+<!-- Text input-->
+       <div class="form-group">
+  <label class="col-md-4 control-label">E-Mail</label>  
+    <div class="col-md-4 inputGroupContainer">
+    <div class="input-group">
+        <span class="input-group-addon"><i class="glyphicon glyphicon-envelope"></i></span>
+  <input name="email" placeholder="E-Mail Address" class="form-control"  type="text">
+    </div>
+  </div>
+</div>
+
+
+<!-- Text input-->
+       
+<div class="form-group">
+  <label class="col-md-4 control-label">Phone #</label>  
+    <div class="col-md-4 inputGroupContainer">
+    <div class="input-group">
+        <span class="input-group-addon"><i class="glyphicon glyphicon-earphone"></i></span>
+  <input name="phone" placeholder="021 234 5678" class="form-control" type="text">
+    </div>
+  </div>
+</div>
+
+<!-- Text input-->
+      
+<div class="form-group">
+  <label class="col-md-4 control-label">Address</label>  
+    <div class="col-md-4 inputGroupContainer">
+    <div class="input-group">
+        <span class="input-group-addon"><i class="glyphicon glyphicon-home"></i></span>
+  <input name="address" placeholder="Address" class="form-control" type="text">
+    </div>
+  </div>
+</div>
+
+<!-- Text input-->
+ 
+<div class="form-group">
+  <label class="col-md-4 control-label">City</label>  
+    <div class="col-md-4 inputGroupContainer">
+    <div class="input-group">
+        <span class="input-group-addon"><i class="glyphicon glyphicon-home"></i></span>
+  <input name="city" placeholder="city" class="form-control"  type="text">
+    </div>
+  </div>
+</div>
+
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-4 control-label">Website or domain name</label>  
+   <div class="col-md-4 inputGroupContainer">
+    <div class="input-group">
+        <span class="input-group-addon"><i class="glyphicon glyphicon-globe"></i></span>
+  <input name="website" placeholder="Website or domain name" class="form-control" type="text">
+    </div>
+  </div>
+</div>
+
+<!-- Text area -->
+  
+<div class="form-group">
+  <label class="col-md-4 control-label">Message</label>
+    <div class="col-md-4 inputGroupContainer">
+    <div class="input-group">
+        <span class="input-group-addon"><i class="glyphicon glyphicon-pencil"></i></span>
+        	<textarea class="form-control" name="comment" placeholder="Send us a message"></textarea>
+  </div>
+  </div>
+</div>
+
+<!-- Success message -->
+<div class="alert alert-success" role="alert" id="success_message">Success <i class="glyphicon glyphicon-thumbs-up"></i> Thanks for contacting us, we will get back to you shortly.</div>
+
+<!-- Button -->
+<div class="form-group">
+  <label class="col-md-4 control-label"></label>
+  <div class="col-md-4">
+    <button type="submit" class="btn btn-warning" >Send <span class="glyphicon glyphicon-send"></span></button>
+  </div>
+</div>
+
+</fieldset>
+</form>
+</div>
+    </div><!-- /.container -->
+<hr>
+</div>
+</div>
+        </div>
+        
+</div>
+<div class="row"> </div>
+    </div> 
+    
+  </div>
+</div>
+<hr>
+<section>
+  <div class="container">
+    <div class="row">
+<div class="container services"> </div>
+  </div>
+</section>
+<hr>
+<div class="container well">
+  <div class="row">
+<div class="col-xs-6 col-sm-6 col-lg-4 col-md-4"> <span class="text-right">
+      </span>
+  <h3>About Us</h3>
+  <hr>
+  <p>Our approach is founded on providing personal service through individual Property Managers with an underlying philosophy of treating your property as if it were our own.</p>
+  <p>Real Estate 7 comprises of an experienced, motivated and specialised team of dedicated people.</p>
+  <p>Our continual striving for excellent customer service is the cornerstone of our success. Our goal is to exceed customer expectations wherever possible and make business with us enjoyable.</p>
+</div>
+<div class="col-xs-6 col-sm-6 col-lg-4 col-md-4 hidden-sm hidden-xs"> <span class="text-right"> </span>
+  <h3>Latest News</h3>
+  <hr>
+  <div class="media-object-default">
+  <div class="media">
+  <div class="media-body">
+        <h4 class="media-heading">Investors</h4>
+	  <p>• Restrictions for investor lending extended from nationwide from Auckland only. <p>• Banks will be forced to require a 40 per cent deposit - up from 30 per cent - for at least 95 per cent of the loans they make in this area.</p></div>
+      <div class="media-right"> <a href="#"> <img class="media-object" src="img/75X.gif" alt="placeholder image"></a></div>
+</div>
+<div class="media">
+  <div class="media-body">
+    <h4 class="media-heading">Home buyers</h4>
+<p>• Restrictions for owner-occupier lending extended from Auckland to nationwide.</p><p>• Required deposit level remains at 20 per cent for at least 90 per cent of bank lending.</p></div>
+  <div class="media-right"> <a href="#"> <img class="media-object" src="img/75X.gif" alt="placeholder image"></a></div>
+</div>
+</div>
+</div>
+<div class="col-xs-6 col-sm-6 col-lg-4 col-md-4"> <span class="text-right"> </span>
+  <h3>Contact Us</h3>
+  <hr>
+  <address>
+  <strong>RealEstate 7</strong><br>
+50 Hazeldead Ave<br>
+Addington<br>
+  </address>
+  <address>
+        <strong>Contact</strong><br>
+        <a href="mailto:#">sales@berclay.co.nz</a>
+      </address>
+      <a class="btn btn-social-icon btn-facebook">
+    <span class="fa fa-facebook-square"></span>
+  </a>
+   <a class="btn btn-social-icon btn-google plus">
+    <span class="fa fa-google-plus-square"></span>
+  </a>
+   </a>
+   <a class="btn btn-social-icon btn-twitter">
+    <span class="fa fa-twitter-square"></span>
+  </a>
+   </a>
+   <a class="btn btn-social-icon btn-linkedin">
+    <span class="fa fa-linkedin-square"></span>
+  </a>
+</div>
+  </div>
+</div>
+<footer class="text-center">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <p><strong>Copyright © Clayton & Elliot. All rights reserved.</strong></p>
+      </div>
+    </div>
+  </div>
+</footer>
+<!-- jQuery --> 
+<script src="js/jquery-1.11.3.min.js"></script> 
+<!-- Bootstrap JS --> -> 
+<script src="js/bootstrap.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<!-- Custom JS -->
+<script src="js/style.js"></script>
+</body>
+</html>
