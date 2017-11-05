5d956a189b4a57b69ac695bc4a93e7d2b06788be
Funce <funce.973@gmail.com>
2017-11-06 09:25:38 +1300

    Update Database with new fields

--- realestate7_database.sql
+++ realestate7_database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 31, 2017 at 10:19 PM
+-- Generation Time: Nov 05, 2017 at 09:24 PM
 -- Server version: 10.1.22-MariaDB
 -- PHP Version: 7.0.18
 
@@ -85,24 +85,25 @@ CREATE TABLE `tbl_property` (
   `p_bed` int(11) NOT NULL,
   `p_bath` int(11) NOT NULL,
   `p_garage` int(11) NOT NULL,
-  `p_price` decimal(10,2) NOT NULL
+  `p_price` decimal(10,2) NOT NULL,
+  `p_type` int(1) NOT NULL COMMENT '1 - Sale, 2 - Rent, 3 - Lease'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tbl_property`
 --
 
-INSERT INTO `tbl_property` (`p_id`, `p_title`, `p_address`, `p_city`, `p_bed`, `p_bath`, `p_garage`, `p_price`) VALUES
-(1, 'Many Rooms & Spaces for the Money', '57 Masefield Street', 'Nelson', 4, 2, 2, '467000.00'),
-(2, 'Two Houses, One Wife…. One Must Go!', '364 Nayland Road', 'Nelson', 4, 2, 2, '500000.00'),
-(3, 'Honest To Goodness Family Living!', '18 Sunningdale Drive', 'Christchurch', 4, 2, 2, '434000.00'),
-(4, 'A Modern Classic', '1 Sunningdale Drive', 'Christchurch', 4, 2, 2, '422000.00'),
-(5, 'Ripe for Reno and Below RV', '83 Princes Drive', 'Dunedin', 3, 2, 2, '341200.00'),
-(6, 'The Work Here Is Done - Move In Ready', '13 Windsor Drive', 'Auckland', 4, 1, 2, '796000.00'),
-(7, 'Quick, put the bubbly on ice!', '396 Princes Drive', 'Dunedin', 3, 2, 2, '332650.00'),
-(8, 'Enduring Nelson Icon', '8 Whitby Road', 'Hamilton', 5, 2, 1, '511000.00'),
-(9, 'Nice N Easy', '35a Vernall Street', 'Nelson', 2, 1, 1, '450000.00'),
-(10, 'Investors! 4 Bedroom Plus Office', '78a Grandview Road', 'Dunedin', 4, 1, 0, '395204.00');
+INSERT INTO `tbl_property` (`p_id`, `p_title`, `p_address`, `p_city`, `p_bed`, `p_bath`, `p_garage`, `p_price`, `p_type`) VALUES
+(1, 'Many Rooms & Spaces for the Money', '57 Masefield Street', 'Nelson', 4, 2, 2, '467000.00', 1),
+(2, 'Two Houses, One Wife…. One Must Go!', '364 Nayland Road', 'Nelson', 4, 2, 2, '500000.00', 2),
+(3, 'Honest To Goodness Family Living!', '18 Sunningdale Drive', 'Christchurch', 4, 2, 2, '434000.00', 3),
+(4, 'A Modern Classic', '1 Sunningdale Drive', 'Christchurch', 4, 2, 2, '422000.00', 1),
+(5, 'Ripe for Reno and Below RV', '83 Princes Drive', 'Dunedin', 3, 2, 2, '341200.00', 1),
+(6, 'The Work Here Is Done - Move In Ready', '13 Windsor Drive', 'Auckland', 4, 1, 2, '796000.00', 2),
+(7, 'Quick, put the bubbly on ice!', '396 Princes Drive', 'Dunedin', 3, 2, 2, '332650.00', 1),
+(8, 'Enduring Nelson Icon', '8 Whitby Road', 'Hamilton', 5, 2, 1, '511000.00', 3),
+(9, 'Nice N Easy', '35a Vernall Street', 'Nelson', 2, 1, 1, '450000.00', 2),
+(10, 'Investors! 4 Bedroom Plus Office', '78a Grandview Road', 'Dunedin', 4, 1, 0, '395204.00', 3);
 
 -- --------------------------------------------------------
 
@@ -134,7 +135,8 @@ INSERT INTO `tbl_wishlist` (`w_id`, `w_p_id`, `w_m_id`) VALUES
 -- Indexes for table `tbl_image`
 --
 ALTER TABLE `tbl_image`
-  ADD PRIMARY KEY (`i_id`);
+  ADD PRIMARY KEY (`i_id`),
+  ADD KEY `i_p_id` (`i_p_id`);
 
 --
 -- Indexes for table `tbl_member`
@@ -152,7 +154,9 @@ ALTER TABLE `tbl_property`
 -- Indexes for table `tbl_wishlist`
 --
 ALTER TABLE `tbl_wishlist`
-  ADD PRIMARY KEY (`w_id`);
+  ADD PRIMARY KEY (`w_id`),
+  ADD KEY `w_p_id` (`w_p_id`),
+  ADD KEY `w_m_id` (`w_m_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -177,7 +181,24 @@ ALTER TABLE `tbl_property`
 -- AUTO_INCREMENT for table `tbl_wishlist`
 --
 ALTER TABLE `tbl_wishlist`
-  MODIFY `w_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;COMMIT;
+  MODIFY `w_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tbl_image`
+--
+ALTER TABLE `tbl_image`
+  ADD CONSTRAINT `tbl_image_ibfk_1` FOREIGN KEY (`i_p_id`) REFERENCES `tbl_property` (`p_id`);
+
+--
+-- Constraints for table `tbl_wishlist`
+--
+ALTER TABLE `tbl_wishlist`
+  ADD CONSTRAINT `tbl_wishlist_ibfk_1` FOREIGN KEY (`w_m_id`) REFERENCES `tbl_member` (`m_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `tbl_wishlist_ibfk_2` FOREIGN KEY (`w_p_id`) REFERENCES `tbl_property` (`p_id`);
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
