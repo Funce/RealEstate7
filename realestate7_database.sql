1dbef8958d3e02089256ba130c5fca8b86bf7739
Funce <funce.973@gmail.com>
2017-11-06 12:30:26 +1300

    Reduce Redundancy in database

--- realestate7_database.sql
+++ realestate7_database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 05, 2017 at 09:24 PM
+-- Generation Time: Nov 06, 2017 at 12:29 AM
 -- Server version: 10.1.22-MariaDB
 -- PHP Version: 7.0.18
 
@@ -21,6 +21,28 @@ SET time_zone = "+00:00";
 --
 -- Database: `realestate7_database`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_city`
+--
+
+CREATE TABLE `tbl_city` (
+  `c_id` int(11) NOT NULL,
+  `c_name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_city`
+--
+
+INSERT INTO `tbl_city` (`c_id`, `c_name`) VALUES
+(1, 'Nelson'),
+(2, 'Christchurch'),
+(3, 'Dunedin'),
+(4, 'Auckland'),
+(5, 'Hamilton');
 
 -- --------------------------------------------------------
 
@@ -81,7 +103,7 @@ CREATE TABLE `tbl_property` (
   `p_id` int(11) NOT NULL COMMENT 'Primary Key',
   `p_title` varchar(50) NOT NULL,
   `p_address` varchar(50) NOT NULL,
-  `p_city` varchar(50) NOT NULL,
+  `p_c_id` int(11) NOT NULL,
   `p_bed` int(11) NOT NULL,
   `p_bath` int(11) NOT NULL,
   `p_garage` int(11) NOT NULL,
@@ -93,17 +115,17 @@ CREATE TABLE `tbl_property` (
 -- Dumping data for table `tbl_property`
 --
 
-INSERT INTO `tbl_property` (`p_id`, `p_title`, `p_address`, `p_city`, `p_bed`, `p_bath`, `p_garage`, `p_price`, `p_type`) VALUES
-(1, 'Many Rooms & Spaces for the Money', '57 Masefield Street', 'Nelson', 4, 2, 2, '467000.00', 1),
-(2, 'Two Houses, One Wife…. One Must Go!', '364 Nayland Road', 'Nelson', 4, 2, 2, '500000.00', 2),
-(3, 'Honest To Goodness Family Living!', '18 Sunningdale Drive', 'Christchurch', 4, 2, 2, '434000.00', 3),
-(4, 'A Modern Classic', '1 Sunningdale Drive', 'Christchurch', 4, 2, 2, '422000.00', 1),
-(5, 'Ripe for Reno and Below RV', '83 Princes Drive', 'Dunedin', 3, 2, 2, '341200.00', 1),
-(6, 'The Work Here Is Done - Move In Ready', '13 Windsor Drive', 'Auckland', 4, 1, 2, '796000.00', 2),
-(7, 'Quick, put the bubbly on ice!', '396 Princes Drive', 'Dunedin', 3, 2, 2, '332650.00', 1),
-(8, 'Enduring Nelson Icon', '8 Whitby Road', 'Hamilton', 5, 2, 1, '511000.00', 3),
-(9, 'Nice N Easy', '35a Vernall Street', 'Nelson', 2, 1, 1, '450000.00', 2),
-(10, 'Investors! 4 Bedroom Plus Office', '78a Grandview Road', 'Dunedin', 4, 1, 0, '395204.00', 3);
+INSERT INTO `tbl_property` (`p_id`, `p_title`, `p_address`, `p_c_id`, `p_bed`, `p_bath`, `p_garage`, `p_price`, `p_type`) VALUES
+(1, 'Many Rooms & Spaces for the Money', '57 Masefield Street', 1, 4, 2, 2, '467000.00', 1),
+(2, 'Two Houses, One Wife…. One Must Go!', '364 Nayland Road', 1, 4, 2, 2, '500000.00', 2),
+(3, 'Honest To Goodness Family Living!', '18 Sunningdale Drive', 2, 4, 2, 2, '434000.00', 3),
+(4, 'A Modern Classic', '1 Sunningdale Drive', 2, 4, 2, 2, '422000.00', 1),
+(5, 'Ripe for Reno and Below RV', '83 Princes Drive', 3, 3, 2, 2, '341200.00', 1),
+(6, 'The Work Here Is Done - Move In Ready', '13 Windsor Drive', 4, 4, 1, 2, '796000.00', 2),
+(7, 'Quick, put the bubbly on ice!', '396 Princes Drive', 1, 3, 2, 2, '332650.00', 1),
+(8, 'Enduring Nelson Icon', '8 Whitby Road', 5, 5, 2, 1, '511000.00', 3),
+(9, 'Nice N Easy', '35a Vernall Street', 1, 2, 1, 1, '450000.00', 2),
+(10, 'Investors! 4 Bedroom Plus Office', '78a Grandview Road', 3, 4, 1, 0, '395204.00', 3);
 
 -- --------------------------------------------------------
 
@@ -130,6 +152,12 @@ INSERT INTO `tbl_wishlist` (`w_id`, `w_p_id`, `w_m_id`) VALUES
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `tbl_city`
+--
+ALTER TABLE `tbl_city`
+  ADD PRIMARY KEY (`c_id`);
 
 --
 -- Indexes for table `tbl_image`
@@ -162,6 +190,11 @@ ALTER TABLE `tbl_wishlist`
 -- AUTO_INCREMENT for dumped tables
 --
 
+--
+-- AUTO_INCREMENT for table `tbl_city`
+--
+ALTER TABLE `tbl_city`
+  MODIFY `c_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `tbl_image`
 --
