b43718cde8eb1450bd7ccc518ad1b8e2b9a4c361
Funce <funce.973@gmail.com>
2017-11-06 15:33:47 +1300

    Make Footer Modular

--- contact.php
+++ contact.php
@@ -239,72 +239,7 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
   </div>
 </section>
 <hr>
-<div class="container well">
-  <div class="row">
-<div class="col-xs-6 col-sm-6 col-lg-4 col-md-4"> <span class="text-right">
-      </span>
-  <h3>About Us</h3>
-  <hr>
-  <p>Our approach is founded on providing personal service through individual Property Managers with an underlying philosophy of treating your property as if it were our own.</p>
-  <p>Real Estate 7 comprises of an experienced, motivated and specialised team of dedicated people.</p>
-  <p>Our continual striving for excellent customer service is the cornerstone of our success. Our goal is to exceed customer expectations wherever possible and make business with us enjoyable.</p>
-</div>
-<div class="col-xs-6 col-sm-6 col-lg-4 col-md-4 hidden-sm hidden-xs"> <span class="text-right"> </span>
-  <h3>Latest News</h3>
-  <hr>
-  <div class="media-object-default">
-  <div class="media">
-  <div class="media-body">
-        <h4 class="media-heading">Investors</h4>
-	  <p>• Restrictions for investor lending extended from nationwide from Auckland only. <p>• Banks will be forced to require a 40 per cent deposit - up from 30 per cent - for at least 95 per cent of the loans they make in this area.</p></div>
-      <div class="media-right"> <a href="#"> <img class="media-object" src="img/75X.gif" alt="placeholder image"></a></div>
-</div>
-<div class="media">
-  <div class="media-body">
-    <h4 class="media-heading">Home buyers</h4>
-<p>• Restrictions for owner-occupier lending extended from Auckland to nationwide.</p><p>• Required deposit level remains at 20 per cent for at least 90 per cent of bank lending.</p></div>
-  <div class="media-right"> <a href="#"> <img class="media-object" src="img/75X.gif" alt="placeholder image"></a></div>
-</div>
-</div>
-</div>
-<div class="col-xs-6 col-sm-6 col-lg-4 col-md-4"> <span class="text-right"> </span>
-  <h3>Contact Us</h3>
-  <hr>
-  <address>
-  <strong>RealEstate 7</strong><br>
-50 Hazeldead Ave<br>
-Addington<br>
-  </address>
-  <address>
-        <strong>Contact</strong><br>
-        <a href="mailto:#">sales@berclay.co.nz</a>
-      </address>
-      <a class="btn btn-social-icon btn-facebook">
-    <span class="fa fa-facebook-square"></span>
-  </a>
-   <a class="btn btn-social-icon btn-google plus">
-    <span class="fa fa-google-plus-square"></span>
-  </a>
-   </a>
-   <a class="btn btn-social-icon btn-twitter">
-    <span class="fa fa-twitter-square"></span>
-  </a>
-   </a>
-   <a class="btn btn-social-icon btn-linkedin">
-    <span class="fa fa-linkedin-square"></span>
-  </a>
-</div>
-  </div>
-</div>
-<footer class="text-center">
-  <div class="container">
-    <div class="row">
-      <div class="col-xs-12">
-        <p><strong>Copyright © Clayton & Elliot. All rights reserved.</strong></p>
-      </div>
-    </div>
-  </div>
-</footer>
+<?php include "footer.php"; ?>
 <!-- jQuery --> 
 <script src="js/jquery-1.11.3.min.js"></script> 
 <!-- Bootstrap JS --> -> 
