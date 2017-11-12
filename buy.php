6a19d62c7cdb0dc82b5bd8a091daad39c88b12a0
Unknown <clayton@berclay.co.nz>
2017-11-12 22:18:49 +1300

    Added a Favicon

--- buy.php
+++ buy.php
@@ -96,6 +96,7 @@
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="shortcut icon" type="image/png" href="img/favicon.png"/>
 <!-- Google Tag Manager -->
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[`0`],
