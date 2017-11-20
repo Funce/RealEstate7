7f3825befbcfcfd469d2602146c518cdedbd5ce4
Funce <funce.973@gmail.com>
2017-11-20 13:06:27 +1300

    Add baseline Admin pages

---
+++ admin/addListing.php
@@ -0,0 +1,42 @@
+<form class="well form-horizontal" method="post" enctype="multipart/form-data" action='#'>
+	<fieldset>
+		<!-- Form Start -->
+		<legend>List a property</legend> 
+
+		<div class="form-group">
+			<label for="dwelling" class="control-label">Please Select</label>
+			<select class="form-control" name="type" id="type1">
+				<option selected="selected" value="0">Region ↓↓↓</option> 
+				<?php
+					$query = "SELECT DISTINCT * FROM tbl_city";
+					$result = mysqli_query($link, $query);
+					while ($row = mysqli_fetch_array($result))
+					{?>
+				<option value="<?php echo $row['c_id'];?>"><?php echo $row['c_name'];?></option>
+					<?php
+					}
+					mysqli_free_result();
+					?>
+			</select>
+		</div>  
+
+		<div class="form-group">
+			<label for="listingtitle" class="control-label">Listing Title</label>
+			<div class="input-group">
+			  <input type="text" class="form-control" name="listing-title" id="listing-title" aria-describedby="basic-addon1">
+			</div>  <br />
+			<h5><strong>Details</strong></h5>
+			<textarea rows="8" cols="25" name="details"></textarea> 
+			<br /><br />
+			<h5><strong>Asking Price</strong></h5>
+			<input type="text" name="price">$
+		</div>
+		
+		<h5><strong>Upload Photos</strong></h5>
+		<ul>          
+			<li><input type="file" name="photos[]" multiple="multiple"></li>
+		</ul>
+		<br>
+		<input type="reset"> <input type="submit">           
+	</fieldset>
+</form>
---
+++ admin/index.php
@@ -0,0 +1,132 @@
+<?php
+	include "../config.php";
+	include "../library/functions.php";
+	include "../library/image-creation.php";
+	// check if logged in
+	if(!isset($_SESSION['user']))
+	{
+		header("Location: ../login.php");
+	}
+	else
+	{
+		if($_SESSION['user']['role'] != ADMIN_LEVEL)
+		{
+			header("Location: ../login.php");
+		}
+	}
+	$error = "";
+
+	// ADD LISTING CHECK
+	if(isset($_POST['type']))
+	{
+		//Check if any fields are empty, and that at least one photo is entered
+		if($_POST[''] or $_POST[''] or $_POST[''] or $_POST[''])
+		{
+			$error = "Please make sure to enter all fields";
+		}
+		else if(isset($_FILES['photos']))
+		{  
+			foreach($_FILES['photos']['tmp_name'] as $key => $tmp_name )
+			{
+      			if(!empty($_FILES['photos']['tmp_name'][$key]))
+				{
+    				$error="Please upload at least one photo";
+    			}
+			}
+		}
+		// All checks succeeded for addListing, lets add it
+		else
+		{
+			
+		}
+	}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KJGS4HC');</script>
+<!-- End Google Tag Manager -->
+<title>Real Estate 7</title>
+
+<!-- Bootstrap -->
+<link rel="stylesheet" type="text/css" media="screen" href="../css/bootstrap.css">
+<link rel="stylesheet" type="text/css" media="screen" href="../css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" media="screen" href="../css/bootstrap-theme.css">
+<link rel="stylesheet" type="text/css" media="screen" href="../css/bootstrap-theme.min.css">
+<link rel="stylesheet" type="text/css" media="screen" href="../css/bootstrap-social.css">
+<!-- Font CSS -->
+<link rel="stylesheet" type="text/css" media="screen" href="../css/font-awesome.css">
+<link rel="stylesheet" type="text/css" media="screen" href="../css/font-awesome.min.css">
+<!-- Custom CSS -->
+<link rel="stylesheet" type="text/css" media="screen" href="../css/style.css">
+<link rel="stylesheet" type="text/css" media="screen" href="../css/tabs.css">
+
+</head>
+<body>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KJGS4HC"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+	<?php include "../navigation.php"; ?>
+	<div class="container">
+	<div class="row">
+	  <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 hidden-xs">
+      </div>    
+		</div>
+  </div>
+<section>
+  <div class="container">
+</section>
+<hr>
+<div class="container">
+  <div class="row">
+    <div class="col-lg-12 col-md-12">
+		<div class="tab">
+			<button id="defaultOpen" class="tablinks" onclick="openTab(event, 'addListing')">Add Listing</button>
+			<button class="tablinks" onclick="openTab(event, 'editListing')">Edit/Remove Listings</button>
+			<button class="tablinks" onclick="openTab(event, 'addCity')">Add City</button>
+			<button class="tablinks" onclick="openTab(event, 'editCity')">Edit/Remove City</button>
+		</div> <!--Tabs-->
+		<div id="addListing" class="tabcontent">
+			<?php include "addListing.php"; ?>
+		</div> <!--Tab addListing-->
+		<div id="editListing" class="tabcontent">
+			<?php include "editListing.php"; ?>
+		</div> <!--Tab editListing-->
+		<div id="addCity" class="tabcontent">
+			<?php include "addCity.php"; ?>
+		</div><!--Tab addCity-->
+		<div id="editCity" class="tabcontent">
+			<?php include "editCity.php"; ?>
+		</div><!--Tab editCity-->
+	</div> 
+  </div>
+</div>
+    <!-- /.container -->
+<hr>
+<?php 
+	include "../footer.php";
+	disconnect();
+?>
+<!-- jQuery --> 
+<script src="../js/jquery-1.11.3.min.js"></script> 
+<!-- Bootstrap JS --> -> 
+<script src="../js/bootstrap.js"></script>
+<script src="../js/bootstrap.min.js"></script>
+<!-- Custom JS -->
+<script src="../js/style.js"></script>
+<script src="../js/tabs.js"></script>
+<script>
+	//Get the element with id="defaultOpen" and click on it
+	document.getElementById("defaultOpen").click();
+</script>
+</body>
+</html>
