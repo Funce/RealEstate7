518a93dc6480c7b19b7a2f6baf70ce1432d38314
Funce <funce.973@gmail.com>
2017-11-21 21:23:17 +1300

    Fix Linkage issue

--- navigation.php
+++ navigation.php
@@ -10,28 +10,28 @@
      <?php
 		if(!isset($_SESSION['user']))
 		{?>
-      	<li><a href="reg.php"><span class="glyphicon glyphicon-user"></span> <strong>Sign Up</strong></a></li>
-      	<li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> <strong>Login</strong></a></li>
+      	<li><a href="<?php echo ROOT_DIRECTORY; ?>reg.php"><span class="glyphicon glyphicon-user"></span> <strong>Sign Up</strong></a></li>
+      	<li><a href="<?php echo ROOT_DIRECTORY; ?>login.php"><span class="glyphicon glyphicon-log-in"></span> <strong>Login</strong></a></li>
       <?php
 		}
 		else 
 		{
 			if($_SESSION['user']['role'] == ADMIN_LEVEL)
 			{?>
-				<li><a href="admin/index.php"><span class="glyphicon glyphicon-wrench"></span> <strong>Admin</strong></a></li>
+				<li><a href="<?php echo ROOT_DIRECTORY; ?>admin/index.php"><span class="glyphicon glyphicon-wrench"></span> <strong>Admin</strong></a></li>
 				<?php
 			}
 		?>
-		<li><a href="wish.php" class="btn btn-warning btn-xs"><strong>Dreamlist</strong></a></li>
-	 	<li><a href="logout.php"><span class="glyphicon glyphicon-log-out"></span> <strong>Logout</strong></a></li>
+		<li><a href="<?php echo ROOT_DIRECTORY; ?>wish.php" class="btn btn-warning btn-xs"><strong>Dreamlist</strong></a></li>
+	 	<li><a href="<?php echo ROOT_DIRECTORY; ?>logout.php"><span class="glyphicon glyphicon-log-out"></span> <strong>Logout</strong></a></li>
 	  
      <?php 
 		}?>
-      <li class="visible-xs-inline"><a href="index.php">Home</a></li>
-      <li class="visible-xs-inline"><a href="buy.php">Browse</a></li>
-      <li class="visible-xs-inline"><a href="post.php">Post a Listing</a></li>
-      <li class="visible-xs-inline"><a href="finance.php">Finance</a></li>
-		<li class="visible-xs-inline"><a href="contact.php">Contact Us</a></li>
+      <li class="visible-xs-inline"><a href="<?php echo ROOT_DIRECTORY; ?>index.php">Home</a></li>
+      <li class="visible-xs-inline"><a href="<?php echo ROOT_DIRECTORY; ?>buy.php">Browse</a></li>
+      <li class="visible-xs-inline"><a href="<?php echo ROOT_DIRECTORY; ?>post.php">Post a Listing</a></li>
+      <li class="visible-xs-inline"><a href="<?php echo ROOT_DIRECTORY; ?>finance.php">Finance</a></li>
+		<li class="visible-xs-inline"><a href="<?php echo ROOT_DIRECTORY; ?>contact.php">Contact Us</a></li>
     </ul>
     </div>
     <!-- /.navbar-collapse --> 
